e0cf3499045b4aa49c219ba76a8bf15ce6021250
seomidia <suporte@seomidia.com.br>
2025-07-05 22:41:02 -0300

    reset

--- resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Bem-vindo</h1>
-@if(Auth::check())
-<a href="/admin/dashboard">Dashboard</a>
-<form method="post" action="{{ route('logout') }}">@csrf<button>Logout</button></form>
-@else
-<a href="{{ route('login') }}">Login</a>
-@endif
--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WebhookController;
-
-Route::post('/kommo/webhook', [WebhookController::class, 'handle']);
