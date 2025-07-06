2f39f2e1c22098dfc5949617868da5d40e8b5b46
seomidia <suporte@seomidia.com.br>
2025-07-05 22:41:48 -0300

    reset

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Dashboard</h1>
-<p>Total de propostas: {{ $count }}</p>
-<ul>
-@foreach($latest as $proposal)
-<li>{{ $proposal->client_name }} - {{ $proposal->amount }}</li>
-@endforeach
-</ul>
--- routes/console.php
+++
@@ -1,4 +0,0 @@
-<?php
-\Illuminate\Support\Facades\Artisan::command('inspire', function () {
-    $this->comment(app(\Illuminate\Foundation\Inspiring::class)->quote());
-})->purpose('Display an inspiring quote');
